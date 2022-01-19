a5aa4497806d3bd4cf650b48a6f05023c6640f8a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-19 21:34:02 +0200

    ADD - entry point

--- PingPongServer/PingPongServer/Program.cs
+++ PingPongServer/PingPongServer/Program.cs
@@ -1,5 +1,6 @@
-﻿    using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using System.Threading;
+using System;
 
 namespace PingPongServer
 {
@@ -7,6 +8,27 @@ namespace PingPongServer
     {
         static async Task Main(string[] args)
         {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken cancellationToken = cts.Token;
+
+            int port;
+
+            if (int.TryParse(args[0], out port))
+            {
+                var server = new Bootstrapper().Bootstrap();
+                Task t = server.RunServer(port, cancellationToken);
+            }
+
+            bool stop = false;
+            do
+            {
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                {
+                    stop = true;
+                }
+            } while (!stop);
+
+            cts.Cancel();
         }
     }
 }
