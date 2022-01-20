505479b5ed43c8db6771f36d5ae688162fd5eb89
BuildTools <unconfigured@null.spigotmc.org>
2022-01-20 12:45:16 +0200

    ADD - private methods send/receive, encapsulation for socket interaction

--- PingPongClient/Client/Client.cs
+++ PingPongClient/Client/Client.cs
@@ -6,10 +6,14 @@ namespace ClientImp
 {
     public class Client<TData>
     {
+        private const string INVALID_INPUT = "invalid input";
+        private const int MAX_BUFFER_SIZE = 1024;
+
         private readonly IClient _client;
 
         private IInput _reader;
         private IOutput _writer;
+        private IInputParser<TData> _parser;
 
         private IInputToRequest<TData> _inputToRequestConverter;
         private IResponseToOutput<TData> _responseToOutputConverter;
@@ -17,12 +21,14 @@ namespace ClientImp
         public Client(IClient client,
                       IInput reader,
                       IOutput writer,
+                      IInputParser<TData> parser,
                       IInputToRequest<TData> inputToRequestConverter,
                       IResponseToOutput<TData> responseToOutputConverter)
         {
             _client = client;
             _reader = reader;
             _writer = writer;
+            _parser = parser;
             _inputToRequestConverter = inputToRequestConverter;
             _responseToOutputConverter = responseToOutputConverter;
         }
@@ -41,6 +47,24 @@ namespace ClientImp
         private void Send()
         {
             string input = _reader.Read();
+            TData data;
+            bool succeeded = _parser.TryParse(input, out data);
+            if (succeeded)
+            {
+                byte[] request = _inputToRequestConverter.GetRequest(data);
+                _client.Send(request);
+            }
+            else
+            {
+                _writer.Write(INVALID_INPUT);
+            }
+        }
+
+        private void Receive()
+        {
+            byte[] response = new byte[MAX_BUFFER_SIZE];
+            TData output = _responseToOutputConverter.Output(response);
+            _writer.Write(output.ToString());
         }
     }
 }
